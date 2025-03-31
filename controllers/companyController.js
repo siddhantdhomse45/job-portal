32b719a1ddc9232234da2c3f2c194b0c8366421d
siddhantdhomse45 <siddh654@gmail.com>
2025-03-31 16:49:08 -0400

    first commit

---
+++ controllers/companyController.js
@@ -0,0 +1,25 @@
+
+
+//Register a new company
+
+export const registerCompany = async(req, res) => {
+
+}
+
+//Company login
+export const loginCompany = async (req, res) => {
+
+}
+
+//Get company data
+export const getCompanyData = async (req, res) => {
+
+}
+
+//Post a new job
+export const postJob = async (req, res) => {
+
+}
+
+//Get Company Job Applicants
+export const get
